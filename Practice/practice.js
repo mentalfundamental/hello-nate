e3d8f8eef000124e6f196ed7e3269920cd1d5b14
Nate Corpuz <nate.corpuz1@gmail.com>
2021-04-01 18:40:38 +0800

    portfolio edit

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,4 +1,12 @@
-let ts = 'testt_ st rinG _onlY2';
-let tr = /\w/g;
-let tryy = ts.match(tr);
-console.log(tryy);
+let burgerBtn = document.getElementById('burger-button');
+let burgerView = document.getElementById('burger');
+burgerBtn.addEventListener('click', burgerClick);
+
+function burgerClick(e) {
+    if (burgerView.style.display === 'none') {
+        burgerView.style.display = 'flex';
+    }
+    else {
+    burgerView.style.display = 'none';
+    }
+}
