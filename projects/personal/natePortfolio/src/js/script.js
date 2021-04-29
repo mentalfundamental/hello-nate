3314d1950c69e8cdf23b72c19d1c311aaceae20c
Nate C <nate.corpuz1@gmail.com>
2021-04-29 20:01:15 +0800

    edit

--- projects/personal/natePortfolio/src/js/script.js
+++ projects/personal/natePortfolio/src/js/script.js
@@ -1,7 +1,11 @@
 let projects = document.querySelector('#options-box ul li:first-child a');
+let transitioner = document.querySelector('#cover');
 
-projects.addEventListener('click', transitioning);
+projects.addEventListener('click', transitionEvent);
 
-function transitioning() {
-    
+function transitionEvent() {
+    transitioner.style.animation = 'slidein 4s ease-in-out forwards';
+    setTimeout(() => {
+        transitioner.style.animation = 'none';
+    }, 4050)
 }
